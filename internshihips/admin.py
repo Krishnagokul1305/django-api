90c1cf011b95ca8a4824c596959f7e76e685112b
krishnagokul1305 <krishnagokul1729@gmail.com>
2026-02-03 11:21:28 +0530

    minor changes

--- internshihips/admin.py
+++ internshihips/admin.py
@@ -1,38 +1,6 @@
 from django.contrib import admin
 from .models import Internship, InternshipRegistration
 
-@admin.register(Internship)
-class InternshipAdmin(admin.ModelAdmin):
-    list_display = ['title', 'event_date', 'is_active', 'created_at']
-    list_filter = ['is_active', 'event_date', 'created_at']
-    search_fields = ['title', 'description']
-    readonly_fields = ['created_at', 'updated_at']
-
-
-@admin.register(InternshipRegistration)
-class InternshipRegistrationAdmin(admin.ModelAdmin):
-    list_display = ['user_email', 'internship_title', 'status', 'applied_at']
-    list_filter = ['status', 'applied_at']
-    search_fields = ['user__email', 'user__name', 'internship__title']
-    readonly_fields = ['applied_at', 'status_updated_at', 'created_at', 'updated_at']
-    fieldsets = (
-        ('Application', {
-            'fields': ('internship', 'user', 'resume_link', 'applied_at', 'status', 'status_updated_at')
-        }),
-        ('Review & Feedback', {
-            'fields': ('rejection_reason',)
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
-
-    def user_email(self, obj):
-        return obj.user.email
-    user_email.short_description = 'User Email'
-
-    def internship_title(self, obj):
-        return obj.internship.title
-    internship_title.short_description = 'Internship'
+admin.site.register(Internship)
+admin.site.register(InternshipRegistration)
 
--- webinars/admin.py
+++ webinars/admin.py
@@ -1,50 +1,5 @@
 from django.contrib import admin
 from .models import Webinar, WebinarRegistration
 
-@admin.register(Webinar)
-class WebinarAdmin(admin.ModelAdmin):
-    list_display = ['title', 'event_date', 'is_active', 'created_at']
-    list_filter = ['is_active', 'event_date', 'created_at']
-    search_fields = ['title', 'description']
-    readonly_fields = ['created_at', 'updated_at']
-
-
-@admin.register(WebinarRegistration)
-class WebinarRegistrationAdmin(admin.ModelAdmin):
-    list_display = ['user_email', 'webinar_title', 'registered_at', 'attended', 'rating', 'feedback_status']
-    list_filter = ['attended', 'rating', 'registered_at', 'feedback_given_at']
-    search_fields = ['user__email', 'user__name', 'webinar__title']
-    readonly_fields = ['registered_at', 'attendance_marked_at', 'feedback_given_at', 'created_at', 'updated_at']
-    fieldsets = (
-        ('Registration', {
-            'fields': ('webinar', 'user', 'registered_at')
-        }),
-        ('Attendance', {
-            'fields': ('attended', 'attendance_marked_at')
-        }),
-        ('Feedback', {
-            'fields': ('rating', 'feedback', 'feedback_given_at')
-        }),
-        ('Rejection', {
-            'fields': ('rejection_reason',)
-        }),
-        ('Additional', {
-            'fields': ('notes',)
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
-
-    def user_email(self, obj):
-        return obj.user.email
-    user_email.short_description = 'User Email'
-
-    def webinar_title(self, obj):
-        return obj.webinar.title
-    webinar_title.short_description = 'Webinar'
-
-    def feedback_status(self, obj):
-        return 'Yes' if obj.feedback_given_at else 'No'
-    feedback_status.short_description = 'Feedback Given'
+admin.site.register(Webinar)
+admin.site.register(WebinarRegistration)
